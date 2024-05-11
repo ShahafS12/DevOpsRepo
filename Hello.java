a78498872a5198f9f4b611915ee91af2c6b34e45
ShahafS12 <104262302+ShahafS12@users.noreply.github.com>
2024-05-11 21:02:57 +0300

    changed print on Hello.java

--- Hello.java
+++ Hello.java
@@ -1,8 +1,7 @@
 public class Hello {
     public static void main (String[] args) {
         for(int i = 0;i<=10;i++) {
-            System.out.println("Hello World..." + i);
-            System.out.println("second print!");
+            System.out.println(" different print (not hello world…) " + i);
         }
     }
 }
