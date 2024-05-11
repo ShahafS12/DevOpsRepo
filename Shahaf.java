a0c5edaf7772df440763396f7db69d0c1406e62d
ShahafS12 <104262302+ShahafS12@users.noreply.github.com>
2024-05-11 21:05:21 +0300

    changed print on Shahaf.java

--- Shahaf.java
+++ Shahaf.java
@@ -2,6 +2,6 @@ public class Shahaf
 {
     public static void main(String[] args)
     {
-        System.out.println("Shahaf several times");
+        System.out.println("Shahaf different print");
     }
 }
